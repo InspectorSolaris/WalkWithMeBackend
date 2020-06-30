facd5538932d2ee37e45d04fd6af1541701c0e7e
German Odilov <german.dark00@gmail.com>
2020-07-01 02:29:41 +0700

    Genetic Added environment for genetic algorithm creation

---
+++ WalkWithMeBackend/Model/Algorithms/Genetic/Genetic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkWithMeBackend.Model.Algorithms.Genetic
+{
+    public class Genetic<T, V>
+    {
+        public Func<Genotype<T, V>> Generator { get; set; }
+
+        public Func<Genotype<T, V>, Genotype<T, V>> Fitness { get; set; }
+
+        public Func<Genotype<T, V>, Genotype<T, V>, Genotype<T, V>> BestOf { get; set; }
+
+        public Func<Genotype<T, V>, Genotype<T, V>, Genotype<T, V>> Crossover { get; set; }
+
+        public Func<Genotype<T, V>, Genotype<T, V>> Mutate { get; set; }
+
+        public Func<List<Genotype<T, V>>, List<(Genotype<T, V> GenotypeA, Genotype<T, V> GenotypeB)>> SelectToCrossover { get; set; }
+
+        public Func<List<Genotype<T, V>>, List<Genotype<T, V>>> SelectToMutation { get; set; }
+
+        public Func<List<Genotype<T, V>>, List<Genotype<T, V>>> SelectToSelection { get; set; }
+
+        public Genotype<T, V> Run(
+            int generationSize,
+            int generationNumber)
+        {
+            if (Fitness is null ||
+                BestOf is null ||
+                Crossover is null ||
+                Mutate is null ||
+                SelectToCrossover is null ||
+                SelectToMutation is null ||
+                SelectToSelection is null)
+            {
+                return new Genotype<T, V>();
+            }
+
+            var generation = new Generation<T, V>(
+                Generator,
+                Fitness,
+                BestOf,
+                Crossover,
+                Mutate,
+                SelectToCrossover,
+                SelectToMutation,
+                SelectToSelection);
+
+            generation.GenerateNew(generationSize);
+
+            for (int i = 0; i < generationNumber; ++i)
+            {
+                generation.Update();
+            }
+
+            return generation.Best;
+        }
+    }
+}
---
+++ WalkWithMeBackend/Model/Algorithms/Genetic/Genotype.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkWithMeBackend.Model.Algorithms.Genetic
+{
+    public class Genotype<T, V>
+    {
+        public Genotype(
+            List<T> genes,
+            V fitness)
+        {
+            this.Genes = genes;
+            this.Fitness = fitness;
+        }
+
+        public Genotype(
+            List<T> genes)
+        {
+            this.Genes = genes;
+        }
+
+        public Genotype()
+        {
+        }
+
+        public List<T> Genes { get; } = new List<T>();
+
+        public V Fitness { get; } = default;
+    }
+}
