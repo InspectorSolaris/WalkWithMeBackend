ef174fde70c4c5141b2c52dd5e96be3539b2e9cf
German Odilov <german.dark00@gmail.com>
2020-07-04 18:08:24 +0700

    Osm Extensions Added useful extensions for Node and Tag

---
+++ WalkWithMeBackend/Extensions/Osm/TagExtension.cs
@@ -0,0 +1,18 @@
+﻿using OsmSharp.Tags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalkWithMeBackend.Extensions.Osm
+{
+    public static class TagExtension
+    {
+        public static bool InTags(
+            this Tag tag,
+            IDictionary<string, IEnumerable<string>> tags)
+        {
+            return tags.ContainsKey(tag.Key) && tags[tag.Key].Contains(tag.Value);
+        }
+    }
+}
