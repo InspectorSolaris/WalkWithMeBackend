6cb0a643e01512be7caf82902aa68f5cd4ed3bc7
German Odilov <german.dark00@gmail.com>
2020-07-05 16:15:23 +0700

    AppUser API turn-off Commented AppUser API Controller to prevent unwanted using for now

--- WalkWithMeBackend/Controllers/AppUsersController.cs
+++ WalkWithMeBackend/Controllers/AppUsersController.cs
@@ -14,108 +14,108 @@ using WalkWithMeBackend.Model.DTO;
 
 namespace WalkWithMeBackend.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AppUsersController : ControllerBase
-    {
-        private UserManager<AppUser> UserManager { get; set; }
+    //[Route("api/[controller]")]
+    //[ApiController]
+    //public class AppUsersController : ControllerBase
+    //{
+    //    private UserManager<AppUser> UserManager { get; set; }
 
-        public AppUsersController(UserManager<AppUser> userManager)
-        {
-            this.UserManager = userManager;
-        }
+    //    public AppUsersController(UserManager<AppUser> userManager)
+    //    {
+    //        this.UserManager = userManager;
+    //    }
 
-        // GET: api/<AppUserController>
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUserDTO>>> Get()
-        {
-            return await UserManager.Users
-                .Select(x => new AppUserDTO(x))
-                .ToListAsync();
-        }
+    //    // GET: api/<AppUserController>
+    //    [HttpGet]
+    //    public async Task<ActionResult<IEnumerable<AppUserDTO>>> Get()
+    //    {
+    //        return await UserManager.Users
+    //            .Select(x => new AppUserDTO(x))
+    //            .ToListAsync();
+    //    }
 
-        // GET api/<AppUserController>/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUserDTO>> Get(string id)
-        {
-            var appUser = await UserManager.FindByIdAsync(id);
-            if (appUser == null)
-            {
-                return NotFound();
-            }
+    //    // GET api/<AppUserController>/5
+    //    [HttpGet("{id}")]
+    //    public async Task<ActionResult<AppUserDTO>> Get(string id)
+    //    {
+    //        var appUser = await UserManager.FindByIdAsync(id);
+    //        if (appUser == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            return new AppUserDTO(appUser);
-        }
+    //        return new AppUserDTO(appUser);
+    //    }
 
-        // POST api/<AppUserController>
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] AppUserDTO appUserDTO)
-        {
-            var appUser = new AppUser
-            {
-                UserName = appUserDTO.UserName,
-                Email = appUserDTO.Email,
-                Created = DateTime.Now
-            };
+    //    // POST api/<AppUserController>
+    //    [HttpPost]
+    //    public async Task<ActionResult> Post([FromBody] AppUserDTO appUserDTO)
+    //    {
+    //        var appUser = new AppUser
+    //        {
+    //            UserName = appUserDTO.UserName,
+    //            Email = appUserDTO.Email,
+    //            Created = DateTime.Now
+    //        };
 
-            var result = await UserManager.CreateAsync(appUser, appUserDTO.Password);
-            if (!result.Succeeded)
-            {
-                return BadRequest();
-            }
+    //        var result = await UserManager.CreateAsync(appUser, appUserDTO.Password);
+    //        if (!result.Succeeded)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            return NoContent();
-        }
+    //        return NoContent();
+    //    }
 
-        // PUT api/<AppUserController>/5
-        [HttpPut("{id}")]
-        public async Task<ActionResult> Put(string id, [FromBody] AppUserDTO appUserDTO)
-        {
-            var appUser = await UserManager.FindByIdAsync(id);
-            if (appUser == null)
-            {
-                return BadRequest();
-            }
+    //    // PUT api/<AppUserController>/5
+    //    [HttpPut("{id}")]
+    //    public async Task<ActionResult> Put(string id, [FromBody] AppUserDTO appUserDTO)
+    //    {
+    //        var appUser = await UserManager.FindByIdAsync(id);
+    //        if (appUser == null)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            appUser.UserName = appUserDTO.UserName;
-            appUser.Email = appUserDTO.Email;
+    //        appUser.UserName = appUserDTO.UserName;
+    //        appUser.Email = appUserDTO.Email;
 
-            if (appUserDTO.Password != null)
-            {
-                var resetToken = await UserManager.GeneratePasswordResetTokenAsync(appUser);
-                var resetResult = await UserManager.ResetPasswordAsync(appUser, resetToken, appUserDTO.Password);
-                if (!resetResult.Succeeded)
-                {
-                    return BadRequest();
-                }
-            }
+    //        if (appUserDTO.Password != null)
+    //        {
+    //            var resetToken = await UserManager.GeneratePasswordResetTokenAsync(appUser);
+    //            var resetResult = await UserManager.ResetPasswordAsync(appUser, resetToken, appUserDTO.Password);
+    //            if (!resetResult.Succeeded)
+    //            {
+    //                return BadRequest();
+    //            }
+    //        }
 
-            var result = await UserManager.UpdateAsync(appUser);
-            if (!result.Succeeded)
-            {
-                return BadRequest();
-            }
+    //        var result = await UserManager.UpdateAsync(appUser);
+    //        if (!result.Succeeded)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            return NoContent();
-        }
+    //        return NoContent();
+    //    }
 
-        // DELETE api/<AppUserController>/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(string id)
-        {
-            var appUser = await UserManager.FindByIdAsync(id);
-            if (appUser == null)
-            {
-                return BadRequest();
-            }
+    //    // DELETE api/<AppUserController>/5
+    //    [HttpDelete("{id}")]
+    //    public async Task<ActionResult> Delete(string id)
+    //    {
+    //        var appUser = await UserManager.FindByIdAsync(id);
+    //        if (appUser == null)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            var result = await UserManager.DeleteAsync(appUser);
-            if (!result.Succeeded)
-            {
-                return BadRequest();
-            }
+    //        var result = await UserManager.DeleteAsync(appUser);
+    //        if (!result.Succeeded)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            return NoContent();
-        }
-    }
+    //        return NoContent();
+    //    }
+    //}
 }
